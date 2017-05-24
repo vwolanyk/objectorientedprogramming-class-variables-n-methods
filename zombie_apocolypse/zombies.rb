26411a3dd805c64018e978727535086f66c9c349
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-23 22:17:09 -0400

    Complete Zombie Exercise - tested works

--- zombie_apocolypse/zombies.rb
+++ zombie_apocolypse/zombies.rb
@@ -66,16 +66,68 @@ class Zombie
       end
   end
 
+  def encounter
 
+    if outrun_zombie? == true
+      return "RUN RUN RUN....You Escaped!!"
+    elsif survive_attack == true
+      return "YOU SURVIVED!!"
+    else
+      new_zombie = Zombie.new(rand(@@max_speed), rand(@@max_strength))
+      @@horde << new_zombie
+      return "Plague!!! You are now a Zombie!"
+    end
+  end
+
+  def outrun_zombie?
+    my_speed = rand(@@max_speed)
+    if my_speed > @speed
+      return true
+    else  return false
+    end
+  end
+
+  def survive_attack
+    if rand(@@max_strength) > @strength
+      return true
+    else return false
+    end
+  end
 
 end
 
-p Zombie.spawn
-p Zombie.all
-p Zombie.all.count
 
-puts "**"
-
-p Zombie.some_die_off
-p Zombie.all.count
-p Zombie.all
+# TEST OUTPUT
+# p Zombie.spawn
+# p Zombie.all
+# p Zombie.all.count
+#
+# p Zombie.spawn
+# p Zombie.spawn
+#
+# puts "**"
+#
+# Zombie.new_day
+# Zombie.new_day
+#
+# Zombie.all.inspect
+#
+# zombie1 = Zombie.all[0]
+# zombie2 = Zombie.all[1]
+# zombie3 = Zombie.all[2]
+#
+# puts zombie1.encounter
+# puts zombie2.encounter
+# puts zombie3.encounter
+# puts "***"
+#
+# Zombie.all.inspect
+# Zombie.all.count
+# Zombie.new_day
+# Zombie.all.count
+#
+# puts "***"
+# puts zombie1.encounter
+# puts zombie2.encounter
+# puts zombie3.encounter
+# puts "***"
